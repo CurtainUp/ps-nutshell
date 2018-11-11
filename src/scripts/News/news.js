9672518ab68ffbed86fda9361a13c6642ac11b3f
Elyse Dawson <dawson.elyse@gmail.com>
2018-11-10 22:28:49 -0600

    Created News class. Able to pass instance through and create innerHTML that creates card below form

--- src/scripts/News/news.js
+++ src/scripts/News/news.js
@@ -1,4 +1,29 @@
 /*
-  author(s):
+  author(s): Elyse
   purpose: defines a class that builds a single news element
 */
+
+class News {
+  constructor(title, summary, timestamp, url) {
+    this.title = title,
+    this.summary = summary,
+    this.timestamp = timestamp
+    this.url = url
+  }
+
+  newsBundler() {
+    let newsSection = document.querySelector("ul.collection")
+    newsSection.innerHTML = `<li class="collection-item avatar">
+    <i class="material-icons circle">subtitles</i>
+    <span class="title"><a href=${this.url}>${this.title}</a></span>
+    <p>${this.summary}</p>
+    <p>${this.timestamp}</p>
+    </li>`
+  }
+}
+
+// let testNews = new News("Nutshell News", "all the happenings at nutshell HQ",
+// "RIGHT NOW", "https://www.vox.com")
+
+// testNews.newsBundler()
+export default News
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,5 @@
 import getFormValues from "./listeners"
+import News from "./News/news"
 
+let testNews = new News("Nutshell News", "all the happenings at nutshell HQ", "RIGHT NOW", "https://www.vox.com")
+testNews.newsBundler()
