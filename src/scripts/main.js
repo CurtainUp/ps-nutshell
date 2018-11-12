c0927644d558d24d4c70950fb231b4ab3e26a592
Nolan-Little <little.nolan@gmail.com>
2018-11-12 14:14:26 -0600

    updated session storage method to store the current page for use in saving the page state on reload

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 import landingPage from "./login/landing"
-
+import userSession from "./sessionStorage"
 if(window.sessionStorage.length === 0) {
   landingPage()
 }
+
+userSession.setCurrentPage("landingPage")
