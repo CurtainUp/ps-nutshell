c415c4909757ab09964a3f57c9cc08f4ab9ff047
Nolan-Little <little.nolan@gmail.com>
2018-11-13 10:01:10 -0600

    added module with function to dynamically create the welcome page

--- src/scripts/login/welcome.js
+++ src/scripts/login/welcome.js
@@ -9,23 +9,19 @@ class Row extends DOMComponent {
   }
 }
 
-// TODO:display name will be set from session storage
-let displayName
-
-const welcomeHeader = new DOMComponent("h1", {classList:"header center"},`Hi ${displayName}`)
-const welcomeMessage = new DOMComponent("h2", { classList: "header center" }, "Welcome to Waddle, where to?")
-const friendNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "friendNavBtn" }, "Friends")
-const toDoNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "toDoNavBtn" }, "To Do")
-const chatNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "chatNavBtn" }, "Chat")
-const buttonRow = new Row({ classList: "row center" }, friendNavBtn, toDoNavBtn, chatNavBtn)
-const eventNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "eventNavBtn" }, "Events")
-const newsNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "newsNavBtn", textContent: "News" }, "News")
-const buttonRow2 = new Row({ classList: "row center" }, eventNavBtn, newsNavBtn)
-const welcomeEl = new DOMComponent("section", {classList:"container landing-page",id:"landingPage" }, welcomeHeader, welcomeMessage, buttonRow, buttonRow2)
-
-
-let welcomePage = () => {
+let welcomePage = (displayName) => {
+  let welcomeHeader = new DOMComponent("h1", {classList:"header center"},`Hi ${displayName}`)
+  let welcomeMessage = new DOMComponent("h2", { classList: "header center" }, "Welcome to Waddle, where to?")
+  let friendNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "friendNavBtn" }, "Friends")
+  let toDoNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "toDoNavBtn" }, "To Do")
+  let chatNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "chatNavBtn" }, "Chat")
+  let buttonRow = new Row({ classList: "row center" }, friendNavBtn, toDoNavBtn, chatNavBtn)
+  let eventNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "eventNavBtn" }, "Events")
+  let newsNavBtn = new DOMComponent("a", { classList: "btn-large waves-effect waves-light", id: "newsNavBtn", textContent: "News" }, "News")
+  let buttonRow2 = new Row({ classList: "row center" }, eventNavBtn, newsNavBtn)
+  let welcomeEl = new DOMComponent("section", {classList:"container landing-page",id:"landingPage" }, welcomeHeader, welcomeMessage, buttonRow, buttonRow2)
   mainContainer.innerHTML = null
+
   welcomeEl.render(".main-container")
 }
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,20 @@
 import landingPage from "./login/landing"
 import welcomePage from "./login/welcome"
+import API from "./api.js"
+import userSession from "./sessionStorage"
+
+userSession.logOutUser()
+userSession.logInUser("2")
 
 if(window.sessionStorage.length === 0) {
   landingPage()
+} else {
+  API.getData("users").then((users) => {
+    users.forEach((user) =>{
+      if(userSession.getUser() === user.id){
+        welcomePage(user.displayName)
+      }
+    })
+  })
 }
 
-welcomePage("penguin")
