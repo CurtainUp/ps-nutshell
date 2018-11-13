f87c27e39f77d066cccc01752bc07ea1713dca5c
Nolan-Little <little.nolan@gmail.com>
2018-11-13 11:55:40 -0600

    complete testing phase of form builder. COnsole logs object to post

--- src/scripts/Task/taskForm.js
+++ src/scripts/Task/taskForm.js
@@ -1,6 +1,7 @@
 import DOMComponent from "nss-domcomponent"
 import Form from "./../formBuilder"
-
+import getFormValues from "../getFormValues"
+import userSession from "../sessionStorage"
 // import Form from "./formBuilder"
 // let inputs = [
 //   ["text", "input1", "This is Input 1"],
@@ -16,7 +17,31 @@ import Form from "./../formBuilder"
 // let form = testForm.build()
 // form.render("body")
 
+const mainContainer = document.querySelector(".main-container")
+
 let taskInputs = [
-  ["text", "task-name", "To Do:"],
-  ["text", "datepicker", "Complete By:"]
+  ["text", "name", "To Do:"],
+  ["text", "dueBy", "Complete By:"],
+  ["button", "saveTaskBtn", "Save Task"]
 ]
+const taskForm = new Form("To Do List", "taskForm", taskInputs).build()
+
+
+let renderTaskForm = () => {
+  mainContainer.innerHTML = null
+  taskForm.render(".main-container")
+
+  let saveTaskBtn = document.querySelector("#saveTaskBtn")
+  saveTaskBtn.setAttribute("formnovalidate", true)
+  document.querySelector("#name").classList.add("input-field")
+  document.querySelector("#dueBy").classList.add("input-field")
+
+  saveTaskBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    let taskObj = getFormValues(e.target.parentNode.parentNode.parentNode)
+    taskObj.status = 0
+    taskObj.userId = userSession.getUser()
+    console.log(taskObj)
+  })
+}
+export default renderTaskForm
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,20 @@ import navListeners from "./listeners"
 import welcomePage from "./login/welcome"
 import API from "./api.js"
 import userSession from "./sessionStorage"
+import renderTaskForm from "./Task/taskForm"
 
 // userSession.logOutUser()
 
 
-if (window.sessionStorage.length === 0) {
-  landingPage()
-} else {
-  API.getData(`users/${userSession.getUser()}`).then((users) => {
-    welcomePage(users.displayName)
-    let navButtons = document.querySelectorAll(".hide")
-    navButtons.forEach((item) => { item.className = "" })
-  })
-}
-navListeners()
+// if (window.sessionStorage.length === 0) {
+//   landingPage()
+// } else {
+//   API.getData(`users/${userSession.getUser()}`).then((users) => {
+//     welcomePage(users.displayName)
+//     let navButtons = document.querySelectorAll(".hide")
+//     navButtons.forEach((item) => { item.className = "" })
+//   })
+// }
+// navListeners()
+
+renderTaskForm()
