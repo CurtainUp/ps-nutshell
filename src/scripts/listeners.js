e54e82a48bb342b3a8a791d69e5c472867519ad2
Elyse Dawson <dawson.elyse@gmail.com>
2018-11-13 11:23:23 -0600

    Added clear.js with function to clear main container or chosen element

--- src/scripts/listeners.js
+++ src/scripts/listeners.js
@@ -2,6 +2,7 @@
   author(s): Elyse
   purpose: Event listeners for section navigation
 */
+import clear from "./clear"
 
 // Grab nav elements
 const friendsNav = document.getElementById("friends")
@@ -16,21 +17,27 @@ const mainContainer = document.querySelector(".main-container")
 // adds event listeners to each nav element
 let navListeners = () => {
   friendsNav.addEventListener("click", () => {
+    clear()
     mainContainer.innerHTML = "<h>Your Friends</h>"
   })
   chatNav.addEventListener("click", () => {
+    clear()
     mainContainer.innerHTML = "<h>Your Chat</h>"
   })
   todoNav.addEventListener("click", () => {
+    clear()
     mainContainer.innerHTML = "<h>Your To Do List</h>"
   })
   eventsNav.addEventListener("click", () => {
+    clear()
     mainContainer.innerHTML = "<h>Your Events</h>"
   })
   newsNav.addEventListener("click", () => {
+    clear()
     mainContainer.innerHTML = "<h>Your News</h>"
   })
   logoutNav.addEventListener("click", () => {
+    clear()
     mainContainer.innerHTML = "<h>GET OUT WE DON'T WANT YOU ANYWAY!</h>"
   })
 }
