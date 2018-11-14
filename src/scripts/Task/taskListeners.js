65964f6dec72bd9768ac7f465a4db8e01f845dfb
Nolan-Little <little.nolan@gmail.com>
2018-11-14 09:51:29 -0600

    can target all appropriate buttons with EL and if statements

--- src/scripts/Task/taskListeners.js
+++ src/scripts/Task/taskListeners.js
@@ -14,74 +14,80 @@ const taskListeners = {
   },
 
   addTaskListeners() {
-    let collection = document.querySelector(".task__list--container")
-    collection.addEventListener("click", (e)=> {
+    let collection = document.querySelector(".main-container")
+    collection.addEventListener("click", (e) => {
       console.log(e.target)
-      if(e.target.classList.contains("delete-button")){
+      if (e.target.classList.contains("delete-button")) {
         console.log("delete")
       }
-      if(e.target.classList.contains("save-button")){
+      if (e.target.classList.contains("save-button")) {
         console.log("save")
       }
-      if(e.target.classList.contains("edit-button")){
+      if (e.target.classList.contains("edit-button")) {
         console.log("edit")
       }
-      if(e.target.classList.contains("status__radio--container")){
+      if (e.target.classList.contains("status__radio--container")) {
         console.log("status")
+      }
+      if (e.target.classList.contains("add-task-button")) {
+        console.log("add task")
+      }
+      if (e.target.classList.contains("clear")) {
+        console.log("close form")
       }
     })
   },
 
-  addStatusListeners() {
-    document.querySelectorAll(".status-radio").forEach((radio) => {
-      radio.addEventListener("click", (e) => {
-        console.log("change status", e)
-        // TODO: patch
-      })
-    })
-  },
+  // addStatusListeners() {
+  //   document.querySelectorAll(".status-radio").forEach((radio) => {
+  //     radio.addEventListener("click", (e) => {
+  //       console.log("change status", e)
+  //       // TODO: patch
+  //     })
+  //   })
+  // },
 
-  addDeleteListener() {
-    document.querySelectorAll(".delete-button").forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        API.deleteData("tasks", e.target.parentElement.id.split("-")[1]).then((response) => {
-          console.log(response)
-        })
-      })
-    })
-  },
+  // addDeleteListener() {
+  //   document.querySelectorAll(".delete-button").forEach((btn) => {
+  //     btn.addEventListener("click", (e) => {
+  //       API.deleteData("tasks", e.target.parentElement.id.split("-")[1]).then((response) => {
+  //         console.log(response)
+  //       })
+  //     })
+  //   })
+  // },
 
-  addEditListener() {
-    document.querySelectorAll(".edit-button").forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        console.log("edit", e)
-        // TODO: edit in place
-      })
-    })
-  },
+  // addEditListener() {
+  //   document.querySelectorAll(".edit-button").forEach((btn) => {
+  //     btn.addEventListener("click", (e) => {
+  //       console.log("edit", e)
+  //       // TODO: edit in place
+  //     })
+  //   })
+  // },
 
-  addSaveListener() {
-    document.querySelectorAll(".save-button").forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        console.log("save", e)
-        // TODO: patch
-      })
-    })
-  },
+  // addSaveListener() {
+  //   document.querySelectorAll(".save-button").forEach((btn) => {
+  //     btn.addEventListener("click", (e) => {
+  //       console.log("save", e)
+  //       // TODO: patch
+  //     })
+  //   })
+  // },
 
-  addAddTaskListener() {
-    document.querySelector(".add-task-button").addEventListener("click", (e) => {
-      document.querySelector("#formContainer").classList.toggle("hide")
-      document.querySelector(".add-task-button").classList.add("hide")
-    })
-  },
+  // addAddTaskListener() {
+  //   document.querySelector(".add-task-button").addEventListener("click", (e) => {
+  //     document.querySelector("#formContainer").classList.toggle("hide")
+  //     document.querySelector(".add-task-button").classList.add("hide")
+  //   })
+  // },
 
-  addCloseFormListener() {
-    document.querySelector(".clear").addEventListener("click", () => {
-      document.querySelector("#formContainer").classList.toggle("hide")
-      document.querySelector(".add-task-button").classList.toggle("hide")
-    })
-  }
+  // addCloseFormListener() {
+  //   document.querySelector(".clear").addEventListener("click", () => {
+  //     document.querySelector("#formContainer").classList.toggle("hide")
+  //     document.querySelector(".add-task-button").classList.toggle("hide")
+  //   })
+  // }
 }
 
 export default taskListeners
